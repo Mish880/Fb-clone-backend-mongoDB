c91f75bc35a56951f7ecc2fcee67004f5196209a
Mish880 <Pereraittec6@gmail.com>
2022-09-02 10:30:40 +0530

    Update account

--- Routes/account.js
+++ Routes/account.js
@@ -23,6 +23,52 @@ router.post('/', async(req,res)=>{
         contactno:req.body.contactno,
         email:req.body.email
     })
+ 
+    try{
+        const response = await account.save()
+        res.json(response)
+    } catch (error) {
+        res.send(error)
 
-   
+    }
 })
+
+router.delete('/:id',async(req,res)=>{
+    try{
+        const account = await Account.findById(req.params.id)
+        const response = await account.remove()
+        res.json(response)
+
+    } catch (error) {
+        res.json(err)
+    }
+})
+
+router.put('/:id',async(req,res)=>{
+    try{
+        const account = await Account.findById(req.params.id)
+        account.firstname = req.body.firstname,
+        account.lastname = req.body.lastname,
+        account.gender = req.body.gender,
+        account.dateofbirth = req.body.dateofbirth,
+        account.password = req.body.password,
+        account.contactno = req.body.contactno,
+        account.email = req.body.email
+
+        const response = await account.save()
+        res.json(response)
+    } catch(error) {
+        res.send(err)
+    }
+})
+
+router.get('/:id',async(req,res)=>{
+    try{
+        const account = await Account.findById(req.params.id)
+        res.json(account)
+    } catch (error) {
+        res.send("Err"+err)
+    }
+})
+
+module.exports = router
